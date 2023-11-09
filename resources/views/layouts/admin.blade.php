2a6775aa0b05d0ba56c11dd0f3dbf5d676a748a6
RiccardoVoltolina <riky.volto@live.com>
2023-11-09 17:52:08 +0100

    general layout implements

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -77,8 +77,8 @@
                                     <i class="fa-solid fa-tachometer-alt fa-lg fa-fw"></i> Dashboard
                                 </a>
 
-                                <a class="nav-link text-white {{ Route::currentRouteName() == 'admin/project' ? 'bg-secondary' : '' }}" href="{{route('project.index')}}">
-                                    <i class="fa-solid fa-tachometer-alt fa-lg fa-fw"></i> Progetti
+                                <a class="nav-link text-white {{ Route::currentRouteName() == 'project.index' ? 'bg-secondary' : '' }}" href="{{route('project.index')}}">
+                                    <i class="fa-solid fa-book-open"></i> Progetti
                                 </a>
                             </li>
 
